5c758855afef9a5cb2eccf61cecda279282e9274
Nguyen Thu Thuy <thuynt20411c@st.uel.edu.vn>
2022-12-05 12:20:59 +0700

    My push layout user infor

---
+++ app/src/main/java/com/project/veggieempiregr5/UserInfor.java
@@ -0,0 +1,14 @@
+package com.project.veggieempiregr5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInfor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_infor);
+    }
+}
