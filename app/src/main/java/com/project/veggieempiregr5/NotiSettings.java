3b83c86cb755a06411320df7c9690ad7b1013e97
UserInfor <myltk20411c@st.uel.edu.vn>
2022-12-06 09:46:57 +0700

    My push

---
+++ app/src/main/java/com/project/veggieempiregr5/NotiSettings.java
@@ -0,0 +1,14 @@
+package com.project.veggieempiregr5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotiSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noti_settings);
+    }
+}
