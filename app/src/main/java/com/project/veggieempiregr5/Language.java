c5c34e53ad51986d56d01edade30ec58518525b7
Nguyen Ngoc Minh Hieu <hieunnm20411c@st.uel.edu.vn>
2022-12-08 14:19:33 +0700

    Hieu

--- app/src/main/java/com/project/veggieempiregr5/Language.java
+++ app/src/main/java/com/project/veggieempiregr5/Language.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.project.veggieempiregr5.databinding.ActivityHelpCenterBinding;
 import com.project.veggieempiregr5.databinding.ActivityLanguageBinding;
@@ -21,7 +24,7 @@ public class Language extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         //======THANH HEADER======
-        getSupportActionBar().setTitle("Cài đặt ngôn ngữ");
+        getSupportActionBar().setTitle("Cài đặt chat");
         Drawable drawable= getResources().getDrawable(R.drawable.ic_baseline_arrow_back_ios_24);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(drawable);
