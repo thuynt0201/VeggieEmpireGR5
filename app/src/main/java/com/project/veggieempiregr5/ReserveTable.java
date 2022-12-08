bdeab45be0091f160b264723dc9599beb96444e5
Lap cua Vo Thu nhớ <thuvta20411c@st.uel.edu.vn>
2022-12-08 13:24:46 +0700

    thu bo sung dat ban

--- app/src/main/java/com/project/veggieempiregr5/ReserveTable.java
+++ app/src/main/java/com/project/veggieempiregr5/ReserveTable.java
@@ -1,9 +1,14 @@
 package com.project.veggieempiregr5;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.project.veggieempiregr5.databinding.ActivityHelpCenterBinding;
 import com.project.veggieempiregr5.databinding.ActivityReserveTableBinding;
@@ -27,6 +32,30 @@ public class ReserveTable extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(drawable);
         //======THANH HEADER END======
 
+
+    }
+
+    public void datban_click(View view) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Đặt bàn thành công !!!");
+        alertDialogBuilder.setIcon(R.drawable.ic_baseline_check_circle_24);
+        alertDialogBuilder.setMessage("Về trang chủ?");
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(ReserveTable.this, TrangChu.class);
+                startActivity(intent);
+            }
+        });
+        alertDialogBuilder.setNeutralButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(ReserveTable.this, "Bạn đã chọn nút hủy", Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
 
     }
 }
